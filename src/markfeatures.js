3734e7b07728e319b06a6824bd60bf81be64d889
Dustin Turpin <dustinturpin@Dustins-MBP.localdomain>
2018-04-19 17:28:30 -0500

    added popup

--- src/markfeatures.js
+++ src/markfeatures.js
@@ -13,41 +13,43 @@ class MarkFeatures extends SdkPopup {
 
   constructor(props) {
     super(props);
-    this.markFeatures = this.markFeatures.bind(this);
+    //this.markFeatures = this.markFeatures.bind(this);
   }
 
-  markFeatures(evt) {
-    const feature_ids = [];
-    const features = this.props.features;
+//   markFeatures(evt) {
+//     const feature_ids = [];
+//     const features = this.props.features;
+//     console.log(features)
 
-    for (let i = 0, ii = features.length; i < ii; i++) {
-      // create an array of ids to be removed from the map.
-      feature_ids.push(features[i].properties.id);
-      // set the feature property to "marked".
-      features[i].properties.isMarked = true;
-    }
+//     for (let i = 0, ii = features.length; i < ii; i++) {
+//       // create an array of ids to be removed from the map.
+//       feature_ids.push(features[i].properties.id);
+//       // set the feature property to "marked".
+//       features[i].properties.isMarked = true;
+//     }
 
-    // remove the old unmarked features
-    //store.dispatch(mapActions.removeFeatures('points', ['in', 'id'].concat(feature_ids)));
-    // add the new freshly marked features.
-    //store.dispatch(mapActions.addFeatures('points', features));
-    // close this popup.
-    this.close(evt);
-  }
+//     // remove the old unmarked features
+//     //store.dispatch(mapActions.removeFeatures('points', ['in', 'id'].concat(feature_ids)));
+//     // add the new freshly marked features.
+//     //store.dispatch(mapActions.addFeatures('points', features));
+//     // close this popup.
+//     this.close(evt);
+//   }
+
 
   render() {
-    const feature_ids = this.props.features.map(f => f.properties.id);
+      let features = this.props.features
+    for (let i = 0 ; i < this.props.features.length; i ++){
+        console.log(features[i].properties['pointsparcels_What address are you at?'])
+    }
+    //const feature_ids = this.props.features.map(f => f.properties.id);
 
     return this.renderPopup((
       <div className="sdk-popup-content">
-        You clicked here:<br />
-        <code>
-          { this.props.coordinate.hms }
-        </code>
         <br />
-        <p>
-          Feature ID(s):<br />
-          <code>{ feature_ids.join(', ') }</code>
+        <div>
+          Feature Info:<br />
+          <p>{ 'TODO' }</p>
           <br />
           <button className="sdk-btn" ref={(c) => {
             if (c) {
@@ -56,7 +58,7 @@ class MarkFeatures extends SdkPopup {
           }}>
             Close
           </button>
-        </p>
+        </div>
       </div>
     ));
   }
